986e010a4f89905d1a20f8735d51c4f57dd2ccd4
Vlad Karpushin <karpushin@ngs.ru>
2019-04-10 15:27:44 +0700

    matlab: added doWmCoding()

--- src matlab/doWmCoding.m
+++ src matlab/doWmCoding.m
@@ -1,28 +1,11 @@
-% 2016-01-06
-% Watermarking in Fourier domain
-% WM is short from watermark
+% function does watermark coding
+function imgOut = doWmCoding(imgOriginal, imgWM, strPathOut)
 
-close all, clc, clear all;
-
-
-% ***************************
-% coder (start)
-% ***************************
-
-SNR = 1/255;    %ampliture of embedded wm
-
-strPathIn = '..\input\';
-strPathOut = '..\output\';
-strFileNameIn = '2.jpg';
-strFileNameInWM = 'nstu1.jpg';
-
-imgOriginal = imread(strcat(strPathIn, strFileNameIn));
 imgOriginal = double(rgb2gray(imgOriginal));
 figure, imshow(uint8(imgOriginal), []);
 title('Original image');
 imwrite(uint8(imgOriginal), strcat(strPathOut, 'img_original.jpg'));
 
-imgWM = imread(strcat(strPathIn, strFileNameInWM));
 imgWM = double(rgb2gray(imgWM));
 figure, imshow(uint8(imgWM), []);
 title('Watermark');
@@ -71,45 +54,5 @@ img_watermarked = imNorm(img_watermarked);
 imwrite(img_watermarked, strcat(strPathOut, 'img_original_plus_wm.bmp'));
 % filtering (stop)
 
-% ***************************
-% coder (stop)
-% ***************************
-
-
-close all, clc, clear all;
-
-% ***************************
-% decoder (start)
-% ***************************
-strPathOut = '..\output\';
-img_watermarked = double(imread(strcat(strPathOut, 'img_original_plus_wm.bmp')));
-figure, imshow(uint8(img_watermarked), []);
-title('An original image with embedded invisible watermark');
-
-[h w] = size(img_watermarked);
-mask = calcMask(h, w);
-img_fft_cut = fft2(img_watermarked) .* mask;
-img_fft_cut(1, 1) = 0;
-
-imgWM = abs(real(ifft2(img_fft_cut))); %demodulation
-imgWM = imNorm(imgWM);
-imwrite(imgWM, strcat(strPathOut, 'wm_extracted.jpg'));
-figure; imshow(imgWM,[]);
-title('An extracted watermark');
-
-% ***************************
-% decoder (stop)
-% ***************************
-% SNR calculation (start)
-SNR
-SNR_Amp_vl = std2(imgD)/std2(imgE_combined) %imgD - modulated signal, 
-SNR_P_v1 = SNR_Amp_vl^2
-SNR_P_DB_v1 = 10*log10(SNR_P_v1) %less accurate
-
-SNR_P_DB_v2 = 10*log10(sum(sum(imgD_PSD))/sum(sum(imgA_PSD))) %by PSD (more accurate)
-
-NSR = std2(imgE_combined)/std2(imgD);
-SNR_Amp_v3 = 1/(NSR-1)
-SNR_P_v3 = SNR_Amp_v3^2
-SNR_P_DB_v3 = 10*log10(SNR_P_v3)
-%SNR calculation (stop)
+imgOut = img_watermarked;
+end
