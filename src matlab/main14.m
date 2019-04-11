2abdd504d044a9437fbf59fec7f2e438fe53fc79
Vlad Karpushin <karpushin@ngs.ru>
2019-04-11 12:06:20 +0700

    matlab: added wm resizing

--- src matlab/main14.m
+++ src matlab/main14.m
@@ -11,13 +11,15 @@ strFileNameOut = 'img_original_plus_wm.bmp';
 % ***************************
 strPathIn = '..\input\';
 strPathOut = '..\output\';
-strFileNameIn = '2.jpg';
+strFileNameIn = '2_1024.jpg';
 strFileNameInWM = 'nstu1.jpg';
 
 SNR = 1/255;                %ampliture of embedded wm
 imgOriginal = imread(strcat(strPathIn, strFileNameIn));
 imgOriginal = double(rgb2gray(imgOriginal));
 imgWM = imread(strcat(strPathIn, strFileNameInWM));
+
+imgWM = imresize(imgWM, size(imgOriginal));
 img_watermarked = doWmCoding(imgOriginal, imgWM, SNR, strPathOut);
 imwrite(img_watermarked, strcat(strPathOut, strFileNameOut));
 
@@ -37,7 +39,8 @@ img_watermarked = double(imread(strcat(strPathOut, strFileNameOut)));
 figure, imshow(uint8(img_watermarked), []);
 title('An original image with embedded invisible watermark');
 
-%img_watermarked = imrotate(img_watermarked, 1, 'bicubic', 'crop'); % robustness
+% robustness / attack
+%img_watermarked = imrotate(img_watermarked, 1, 'bicubic', 'crop'); 
 %img_watermarked = imresize(img_watermarked, 0.95); % robustness
 %img_watermarked = img_watermarked(1:1000,1:1000);
 
@@ -47,7 +50,7 @@ title('rotated');
 imgWM_extracted = doWmDeCoding(img_watermarked);
 
 imwrite(imgWM_extracted, strcat(strPathOut, 'wm_extracted.jpg'));
-figure; imshow(imgWM_extracted,[]);
+figure, imshow(imgWM_extracted,[]);
 title('An extracted watermark');
 
 % ***************************
